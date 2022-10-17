829b67f9acc75d4bdbed9c00228a641f78d65cf1
wangweifcc <36262338+wangweifcc@users.noreply.github.com>
2022-10-17 09:51:00 +0800

    Delete y_flash_minibatch_d2d_cyc directory

--- y_flash_minibatch_d2d_cyc/erase_yflash.m
+++
@@ -1,5 +0,0 @@
-function [pa,Qfg,G]=erase_yflash(pa,Qfg,tp)
-    [Qfg, ~, ~, ~] = yflash(pa,Qfg, 0, 'z', 8, tp);
-    [~, i2v, ~, ~] = yflash(pa,Qfg, 2, 0, 0, 0);
-    pa.pa_i.beta = pa.pa_i.beta+(pa.cyc.beta_max-pa.pa_i.beta)*(exp(tp/pa.cyc.tau_beta)-1);
-    G=i2v/2;
--- y_flash_minibatch_d2d_cyc/read_yflash.m
+++
@@ -1,2 +0,0 @@
-function i2v=read_yflash(Qfg)
-    [~, i2v, ~, ~] = yflash(Qfg, 2, 0, '0', 0);
